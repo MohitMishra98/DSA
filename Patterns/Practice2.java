d03edddc89cb96bd9fabc3ce8be69f1f05d5a05c
Mohit Mishra <150663253+MohitMishra98@users.noreply.github.com>
2024-05-27 16:18:12 +0000

    A A A A A

--- Patterns/Practice2.java
+++ Patterns/Practice2.java
@@ -1,15 +1,13 @@
-public class Practice1 {
+package Patterns;
+public class Practice2 {
     public static void main(String args[]){
-        
+
         for(int i=1; i<=10; i++){
-            for(int j=1; j<=i; j++){
+            for(int j=10; j>=i; j--){
                 System.out.print("*");
             }
             System.out.println();
         }
-
-        // i is for lines
-        // j is for stars
 
     }
     
--- Patterns/Practice3.java
+++ Patterns/Practice3.java
@@ -1,9 +1,10 @@
-public class Practice2 {
+package Patterns;
+public class Practice3 {
     public static void main(String args[]){
 
         for(int i=1; i<=10; i++){
-            for(int j=10; j>=i; j--){
-                System.out.print("*");
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
             }
             System.out.println();
         }
