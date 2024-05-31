ba8d4675223ed5607524d38e2e86b2f26a577439
Mohit Mishra <150663253+MohitMishra98@users.noreply.github.com>
2024-05-31 16:37:44 +0000

    A A A A A

--- Patterns/Pattern2.java
+++ Patterns/Pattern2.java
@@ -17,6 +17,7 @@ public class Pattern2 {
         }
 
         // to reverse the pattern just change the outer loop
+        
         for(int i=n; i>=1; i--){
             for(int j=1; j<=i; j++){
                 System.out.print("*");
