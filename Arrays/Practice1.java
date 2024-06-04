b0c40094ae3e3b746fa733bdba03cdbd60f99d83
Mohit Mishra <150663253+MohitMishra98@users.noreply.github.com>
2024-06-04 15:15:24 +0000

    A A A A A

--- Arrays/Practice1.java
+++ Arrays/Practice1.java
@@ -3,19 +3,20 @@ public class Practice1 {
 
     public static void subArray(int numbers[]){
 
-        for(int i=0; i<numbers.length; i++){ // counts the start
+        for(int i=0; i<numbers.length; i++){     // counts the start
             for(int j=i; j<numbers.length; j++){ // counts the end
                 for(int k=i; k<=j; k++){
                     System.out.print(numbers[k] + " "); // prints the array from start to end
                 }
             }
             System.out.println();
-        }
+        }       
     }
     public static void main(String args[]){
         int numbers[] = {90,91,92,93,94,95};
 
         subArray(numbers);
+        
     }
     
 }
