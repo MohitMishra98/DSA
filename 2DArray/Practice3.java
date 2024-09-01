fdc24f143044bbe0cfcea166e445907c75f31745
Mohit Mishra <150663253+MohitMishra98@users.noreply.github.com>
2024-09-01 11:18:29 +0000

    AAAAAAAAAA

--- 2DArray/Practice3.java
+++ 2DArray/Practice3.java
@@ -1,40 +1,34 @@
 public class Practice3 {
 
-    public static void spiral(int matrix[][]){
-        int m = matrix[0].length;
-        int n = matrix.length;
-
-
+    public static void spiral(int arr[][]){
         int startRow = 0;
-        int endRow = n-1;
         int startCol = 0;
-        int endCol = n-1;
+        int endRow = arr.length-1;
+        int endCol = arr[0].length-1;
+
+        System.out.println("Here is spiral Matrix : ");
 
         while(startRow <= endRow && startCol <= endCol){
-            // Top 
-            for(int j=startCol; j<=endCol; j++){
-                System.out.println(matrix[startRow][j] + " ");
+            
+            for(int i=startCol; i<= endCol; i++){
+                System.out.println(arr[startRow][i]);
             }
-
-            // Right
-            for(int i=startRow-1; i<=endRow; i++){
-                System.out.println(matrix[i][endCol] + " ");
-            }
-
-            // bottom
-            for(int j=endCol-1; j>=startCol; j--){
-                System.out.println(matrix[endRow][j] + " ");
-            }
-
-            // 
-            for(int i=endRow-1; i>=startRow+1; i--){
-                System.out.println(matrix[i][startCol] + " ");
-            }
-
             startRow++;
-            endRow--;
-            startCol++;
+
+            for(int i=startRow; i<=endRow; i++){
+                System.out.println(arr[i][endCol]);
+            }
             endCol--;
+
+            for(int i=endCol; i>=startCol; i--){
+                System.out.println(arr[endRow][i]);
+            }
+            endRow--;
+
+            for(int i=endRow; i>=startRow; i--){
+                System.out.println(arr[i][startCol]);
+            }
+            startCol++;
         }
     }
     public static void main(String args[]){
